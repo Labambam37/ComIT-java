2411edd2f2aee48e94b45bf236f182f2c1bd4969
Labambam37 <labakebhadmus@gmail.com@gmail.com>
2024-02-10 14:44:16 -0600

    first commit

---
+++ src/main/java/Org/comIT/course/App.java
@@ -0,0 +1,13 @@
+package Org.comIT.course;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! HI" );
+    }
+}
